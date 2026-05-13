470756fdaade937c21f70b45a1abdcb42ae43938
4alvit <4alvit@github.com>
2026-05-13 16:16:14 -0700

    Versions control in the code

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    // Read VERSION file
+    let version = std::fs::read_to_string("VERSION")
+        .expect("Failed to read VERSION file")
+        .trim()
+        .to_string();
+
+    // Set version for Cargo
+    println!("cargo:rustc-env=CARGO_PKG_VERSION={}", version);
+
     tauri_build::build()
 }
