19a3b3373b87d549066b68af515d006e114bc461
4alvit <4alvit@github.com>
2026-05-16 01:24:18 -0700

    VueAwesomeButton implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueAwesomeButton from 'vue-awesome-button'
+import 'vue-awesome-button/dist/style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAwesomeButton)
+app.mount('#app')
---
+++ src/types/vue-awesome-button.d.ts
@@ -0,0 +1,5 @@
+declare module 'vue-awesome-button' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<any, any, any>
+  export default component
+}
