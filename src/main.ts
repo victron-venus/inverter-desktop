b341e65994da875e73d9275104d1c1e74b782d60
4alvit <4alvit@github.com>
2026-05-16 02:06:37 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import VueAwesomeButton from 'vue-awesome-button'
-import 'vue-awesome-button/dist/style.css'
 
 const app = createApp(App)
 app.component('VueAwesomeButton', VueAwesomeButton)
