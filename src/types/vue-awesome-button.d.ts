d1e41b272f7d3deda4390270bb1817243c89a7d3
4alvit <4alvit@github.com>
2026-05-16 02:42:00 -0700

    Update vue-awesome-button.d.ts

--- src/types/vue-awesome-button.d.ts
+++ src/types/vue-awesome-button.d.ts
@@ -1,5 +1,6 @@
 declare module 'vue-awesome-button' {
   import { DefineComponent } from 'vue'
-  const component: DefineComponent<any, any, any>
-  export default component
+  const AwesomeButton: DefineComponent<any, any, any>
+  export default AwesomeButton
+  export { AwesomeButton }
 }
